84ac5b2ba204041f214b9671c2efa1f54ef1efd2
dmithorzh <dmithor@yandex.ru>
2022-02-08 15:02:12 +0300

    Added jpa annotation to BaseEntity

--- pet-clinic-data/src/main/java/com/example/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/BaseEntity.java
@@ -1,12 +1,18 @@
 package com.example.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by Dim@$$ik on 25.01.22.
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
