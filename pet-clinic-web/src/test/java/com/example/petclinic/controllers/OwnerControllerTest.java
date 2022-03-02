f022204ca5c138dbd56cb9ed28687ab775ade752
dmithorzh <dmithor@yandex.ru>
2022-03-02 14:45:49 +0300

    Added crud templates for owners

--- pet-clinic-web/src/test/java/com/example/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/example/petclinic/controllers/OwnerControllerTest.java
@@ -14,7 +14,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -49,12 +50,32 @@ class OwnerControllerTest {
                 .andExpect(model().attribute("owners", hasSize(2)));
 
     }
+    @Test
+    void ownersListbyIndex() throws Exception {
+        when(ownerService.findAll()).thenReturn(owners);
+
+        mockMvc.perform(get("/owners/index"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/index"))
+                .andExpect(model().attribute("owners", hasSize(2)));
+
+    }
 
     @Test
     void findOwners() throws Exception {
         mockMvc.perform(get(("/owners/find")))
                 .andExpect(status().isOk())
                 .andExpect(view().name("notimplemented"));
+
+    }
+    @Test
+    void showOwners() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1l))));
 
     }
 }
