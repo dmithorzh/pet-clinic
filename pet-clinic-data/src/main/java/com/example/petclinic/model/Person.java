261db57a9b73621b39bd8112ec55ad3c301005d3
dmithorzh <dmithor@yandex.ru>
2022-03-02 16:17:36 +0300

    implemented find owners

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Person.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
@@ -19,8 +20,10 @@ public class Person extends BaseEntity {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name ="last_name")
     private String lastName;
 
 
