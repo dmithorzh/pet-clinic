b1b8cbcbd01049b84e213cc59090c837a42d30d3
dmithorzh <dmithor@yandex.ru>
2022-03-04 13:21:38 +0300

    added VisitController

---
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/VisitController.java
@@ -0,0 +1,59 @@
+package com.example.petclinic.controllers;
+
+import com.example.petclinic.model.Pet;
+import com.example.petclinic.model.Visit;
+import com.example.petclinic.services.PetService;
+import com.example.petclinic.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+public class VisitController {
+
+    private final VisitService visitService;
+    private final PetService petService;
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable ("petId") Long petId, Model model){
+        Pet pet = this.petService.findById(petId);
+        model.addAttribute("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        return visit;
+
+    }
+
+    @GetMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable Long petId, Model model) {
+        return "pets/createOrUpdateVisitForm";
+    }
+
+
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(@Valid Visit visit, BindingResult result) {
+        if (result.hasErrors()) {
+            return "pets/createOrUpdateVisitForm";
+        } else {
+            visitService.save(visit);
+        }
+        return "redirect:/owners/{ownerId}";
+    }
+
+
+
+}
